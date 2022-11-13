4ae8d00b8496df9c8797075082fa209f6ab63a47
Poornima <poornimamuralip@gmail.com>
2022-11-13 22:58:39 +0530

    route for comapny.js

--- routes/company.js
+++ routes/company.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var router = express.Router();
-const { company, companySignUp } = require("../controllers/companyController");
+const { company, companySignUp, companyLogin } = require("../controllers/companyController");
 
 router.get("/", company);
 router.get("/signup", companySignUp);
-
+router.get("/login",companyLogin)
 module.exports = router;
