2004a6e766284d3ed4f283de5852809a895542c2
Poornii <poornimamuralip@gmail.com>
2022-11-14 13:26:27 +0530

    cretaed companycontroll function

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,11 +1,47 @@
+const companyModel = require("../models/companyModel");
+const bcrypt = require("bcrypt");
+
 const company = function (req, res) {
   res.send("Company Page");
 };
 const companySignUp = function (req, res) {
   res.render("company/companySignUp");
 };
+const companyLogin = function (req, res) {
+  res.render("company/companyLogin");
+};
+
+const signupCompany = async function (req, res) {
+  console.log(req.body);
+  try {
+    req.body.Password = await bcrypt.hash(req.body.Password, 10);
+    await companyModel.create(req.body);
+  } catch (error) {
+    console.log(error);
+  }
+};
+const loginCompany = async function (req, res) {
+  console.log(req.body);
+  let company = await companyModel.findOne({ Email: req.body.Email });
+  if (company != null) {
+    console.log(company.Password);
+    console.log(req.body.Password);
+    let check = await bcrypt.compare(req.body.Password, company.Password);
+    console.log(check);
+    if (check) {
+      req.session.company = company;
+      console.log(req.session.company);
+      res.redirect("/company/");
+    } else {
+      res.redirect("/company/login");
+    }
+  }
+};
 
 module.exports = {
   company,
   companySignUp,
+  companyLogin,
+  signupCompany,
+  loginCompany,
 };
---
+++ models/companyModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const companySchema = mongoose.Schema({
+  Name: {
+    type: String,
+    maxLength: 20,
+    required: [true, "You must enter name"],
+  },
+  Email: {
+    type: String,
+    maxLength: 50,
+    required: true,
+    unique: true,
+  },
+  Password: {
+    type: String,
+    maxLength: 200,
+    required: true,
+  },
+  Phone: {
+    type: Number,
+    maxLength: 10,
+    required: true,
+  },
+});
+module.exports = mongoose.model("company", companySchema);
