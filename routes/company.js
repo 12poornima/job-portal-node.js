59099f7ccc0abb08f83b27f08df70d19fe9419fc
Poornima <poornimamuralip@gmail.com>
2022-11-20 19:38:41 +0530

    home page company category completed

--- routes/company.js
+++ routes/company.js
@@ -1,3 +1,36 @@
+// // swiper
+// var swiper=new Swiper(".mySwiper",
+// {
+//   loop:true,
+//   autoplay:{
+//     delay:2500,
+//     disableOnInteraction:false,
+//   },
+//   slidesPerView:1,
+//   spaceBetween:10,
+//   pagination:{
+//     el:".swper-pagination",
+//     clickable:true
+//   },
+//   breakpoints:{
+//     640:{
+//       slidesPerView:2,
+//       spaceBetween:20,
+//     },
+//     768:{
+//       slidesPerView:3,
+//       spaceBetween:40,
+//     },
+//     1024:{
+//       slidesPerView:3,
+//       spaceBetween50,
+//     }
+//   }
+// })
+
+
+
+
 var express = require("express");
 var router = express.Router();
 const {
